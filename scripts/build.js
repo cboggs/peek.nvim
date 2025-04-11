c0a55035aa312ca9e5ce865d6d95e77209449e7c
cody.boggs <cody.boggs@instabase.com>
2025-04-11 23:49:33 +0000

    Bump mermaid to 11.6.0

--- scripts/build.js
+++ scripts/build.js
@@ -69,10 +69,7 @@ const result = await Promise.allSettled([
     },
   ),
 
-  download(
-    'https://cdn.jsdelivr.net/npm/mermaid@11.5.0/dist/mermaid.min.js',
-    'public/mermaid.min.js',
-  ),
+  download('https://cdn.jsdelivr.net/npm/mermaid@11.6.0/dist/mermaid.js', 'public/mermaid.min.js'),
 
   download('https://cdn.jsdelivr.net/npm/katex@0.16.9/dist/katex.min.css', 'public/katex.min.css'),
 
